97c3b5e7332691fd8939f9517534a7e3a3134a88
podxboq <podxboq@gmail.com>
2012-11-02 00:55:23 +0100

    Add support for google music player

---
+++ products/music_support.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include the minimim APK files, all possible .xml, .jar and .so files
+# Only the necessary files for use Google Account
+
+########################################################################
+# Music support
+########################################################################
+
+PRODUCT_COPY_FILES := \
+vendor/google/gapps/prebuilt/system/app/com.google.android.music.apk:system/app/com.google.android.music.apk
